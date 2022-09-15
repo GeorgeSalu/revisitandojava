78c6c0e3b8810755fc0476ff4968d936782ab19c
GeorgeSalu <george.salu10@gmail.com>
2022-09-15 11:53:10 -0300

    fundamentos - notacao ponto

---
+++ exercicios/src/main/java/fundamentos/NotacaoPonto.java
@@ -0,0 +1,10 @@
+package fundamentos;
+
+public class NotacaoPonto {
+	public static void main(String[] args) {
+		String s = "bom dia X";
+		s = s.replace("X", "senhora");
+		s = s.toUpperCase();
+		System.out.println(s);
+	}
+}
