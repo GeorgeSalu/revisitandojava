d428bff403712cb044d697c07b796fe9fa837808
GeorgeSalu <george.salu10@gmail.com>
2022-09-15 15:24:50 -0300

    fundamentos - desafio operadores logicos

---
+++ exercicios/src/main/java/fundamentos/DesafioLogico.java
@@ -0,0 +1,12 @@
+package fundamentos;
+
+public class DesafioLogico {
+	public static void main(String[] args) {
+		boolean trabalho1 = false;
+		boolean trabalho2 = false;
+		
+		boolean comprouTv50 = trabalho1 && trabalho2;
+		
+		System.out.println("Comprou tv "+comprouTv50);
+	}
+}
