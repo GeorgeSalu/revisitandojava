5bed8e2e5869e547aed01469a45a30103733ceda
GeorgeSalu <george.salu10@gmail.com>
2022-09-15 14:26:51 -0300

    fundamentos - conversao string numero

---
+++ exercicios/src/main/java/fundamentos/ConversaoStringNumero.java
@@ -0,0 +1,18 @@
+package fundamentos;
+
+import javax.swing.JOptionPane;
+
+public class ConversaoStringNumero {
+	public static void main(String[] args) {
+		String valor1 = JOptionPane.showInputDialog("Digite o primeiro valor");
+		String valor2 = JOptionPane.showInputDialog("Digite o segundo valor");
+		
+		double numero1 = Double.parseDouble(valor1);
+		double numero2 = Double.parseDouble(valor2);
+		
+		double soma = numero1 + numero2;
+		
+		System.out.println("soma é "+soma);
+		
+	}
+}
