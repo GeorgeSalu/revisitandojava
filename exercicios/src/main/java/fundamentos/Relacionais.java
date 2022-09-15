ffc1866a1fac33cebca364638d1b22b9f4d5c9bc
GeorgeSalu <george.salu10@gmail.com>
2022-09-15 15:30:34 -0300

    fundamentos - operadores relacionais

---
+++ exercicios/src/main/java/fundamentos/Relacionais.java
@@ -0,0 +1,18 @@
+package fundamentos;
+
+public class Relacionais {
+	public static void main(String[] args) {
+		
+		int a = 97;
+		int b = 'a';
+		
+		System.out.println(a == b);
+		System.out.println(a > b);
+		System.out.println(a >= b);
+		System.out.println(a < b);
+		System.out.println(a <= b);
+		System.out.println(a != b);
+
+		
+	}
+}
