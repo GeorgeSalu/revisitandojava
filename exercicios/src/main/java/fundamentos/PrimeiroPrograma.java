fb508f66a36edb2ed116555a65556beaa340ce73
GeorgeSalu <george.salu10@gmail.com>
2022-09-15 10:57:41 -0300

    fundamentos - comentarios

--- exercicios/src/main/java/fundamentos/PrimeiroPrograma.java
+++ exercicios/src/main/java/fundamentos/PrimeiroPrograma.java
@@ -2,7 +2,16 @@ package fundamentos;
 
 public class PrimeiroPrograma {
 	public static void main(String[] args) {
+		
+		// uma sentença de codigo termina com ;
 		System.out.println("primeiro programa parte #01");
+		
+		/*
+		 
+		  Linha 1 
+		  Linha 2
+		  
+		 */
 		System.out.println("primeiro programa parte #02");
 	}
 }
