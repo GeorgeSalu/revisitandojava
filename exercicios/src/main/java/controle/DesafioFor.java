f13dffef0032adf5745414c95c5b230b64042637
GeorgeSalu <george.salu10@gmail.com>
2022-09-16 14:38:47 -0300

    estrutura de controle - desafio for

---
+++ exercicios/src/main/java/controle/DesafioFor.java
@@ -0,0 +1,15 @@
+package controle;
+
+public class DesafioFor {
+	public static void main(String[] args) {
+		
+		String valor = "#";
+		for(int i = 1;i <= 5;i++) {
+			System.out.println(valor);
+			valor += "#";
+		}
+		
+		//versao do desafio
+		//não pode usar valor numerico pra controlar o laco
+	}
+}
