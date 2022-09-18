e9fa7dd49e1a5da07dba1cbb6db01daa8d1d8ee2
GeorgeSalu <george.salu10@gmail.com>
2022-09-18 14:30:46 -0300

    classes e metodos - classe produto

---
+++ exercicios/src/main/java/classe/ProdutoTeste.java
@@ -0,0 +1,23 @@
+package classe;
+
+public class ProdutoTeste {
+	public static void main(String[] args) {
+		Produto p1 = new Produto();
+		p1.nome = "notebook";
+		p1.preco = 12132;
+		p1.desconto = 0.25;
+		
+		var p2 = new Produto();
+		p2.nome = "caneta preta";
+		p2.preco = 12.33;
+		p2.desconto = 0.29;
+		
+		System.out.println(p2.nome);
+		System.out.println(p1.nome);
+		
+		double precoFinal1 = p1.preco * (1 - p1.desconto);
+		double precoFinal2 = p2.preco * (1 - p2.desconto);
+		
+		System.out.println((precoFinal1 + precoFinal2) / 2);
+	}
+}
