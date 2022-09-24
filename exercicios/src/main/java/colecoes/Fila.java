8c0f684781269f2a267562e33b90350b985aa060
GeorgeSalu <george.salu10@gmail.com>
2022-09-24 15:58:07 -0300

    classes e metodos - fila

---
+++ exercicios/src/main/java/colecoes/Fila.java
@@ -0,0 +1,27 @@
+package colecoes;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Fila {
+	public static void main(String[] args) {
+		
+		Queue<String> fila = new LinkedList<>();
+		
+		/*
+		 * offer e add => adicionam elementos na fila a diferença é o 
+		 * comportamento quando a fila esta cheia
+		*/
+		fila.add("Ana");
+		fila.offer("Bia");
+		fila.add("Carlos");
+		fila.offer("Daniel");
+		fila.add("Rafaela");
+		fila.offer("Gui");
+		
+		System.out.println(fila.peek());
+		System.out.println(fila.peek());
+		System.out.println(fila.element());
+		
+	}
+}
