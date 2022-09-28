72891d6cb2b6a963711c135bdc8bebb33f5efa4d
GeorgeSalu <george.salu10@gmail.com>
2022-09-28 10:37:39 -0300

    orientação a objetos - desafio composicao - resposta

--- exercicios/src/main/java/oo/composicao/desafio/Cliente.java
+++ exercicios/src/main/java/oo/composicao/desafio/Cliente.java
@@ -5,8 +5,8 @@ import java.util.List;
 
 public class Cliente {
 	
-	String nome;
-	List<Compra> compras = new ArrayList<>();
+	final String nome;
+	final List<Compra> compras = new ArrayList<>();
 	
 	Cliente(String nome) {
 		this.nome = nome;
