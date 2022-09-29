5e4c2103930467155a94017358560f68ee85037f
GeorgeSalu <george.salu10@gmail.com>
2022-09-29 11:53:22 -0300

    orientação a objetos - desafio heranca

--- exercicios/src/main/java/oo/heranca/desafio/Ferrari.java
+++ exercicios/src/main/java/oo/heranca/desafio/Ferrari.java
@@ -2,6 +2,15 @@ package oo.heranca.desafio;
 
 public class Ferrari extends Carro {
 
+	public Ferrari() {
+		super(315);
+	}
+	
+	public Ferrari(int velocidadeMaxima) {
+		super(velocidadeMaxima);
+		delta = 15;
+	}
+	
 	@Override
 	void acelerar() {
 		velocidadeAtual += 15;
