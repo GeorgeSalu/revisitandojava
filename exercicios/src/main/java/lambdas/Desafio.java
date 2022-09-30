a131a76dada6ff0501d6d75ea80e5a19cce4f65d
GeorgeSalu <george.salu10@gmail.com>
2022-09-30 16:03:30 -0300

    lambdas - desafio / resposta

--- exercicios/src/main/java/lambdas/Desafio.java
+++ exercicios/src/main/java/lambdas/Desafio.java
@@ -6,13 +6,12 @@ import java.util.function.UnaryOperator;
 public class Desafio {
 	public static void main(String[] args) {
 		
-		Produto p = new Produto("ipad", 3235.89, 0.19);
+		Produto p = new Produto("ipad", 3235.89, 0.13);
 		
 		/*
 		 1. a partir do produto calcular o preco real (com desconto)
 		 2. imposto municipal: >= 2500 (8.5%) / < 2500 (isento)
 		 3. frete: >= 3000 (100) / < 3000 (50)
-		 4. arredondar: deixar duas casas decimais
 		 5. formatar: r$ 1234.56 
 		*/
 		Function<Produto, Double> precoFinal = 
@@ -22,7 +21,7 @@ public class Desafio {
 		UnaryOperator<Double> frete = 
 				preco -> preco >= 3000 ? preco + 100 : preco + 50;
 		Function<Double, String> formatar = 
-				preco -> ("R$"+ preco).replace(".", ",");
+				preco -> ("R$ "+ preco).replace(".", ",");
 		
 		String preco = precoFinal
 				.andThen(impostoMunicipal)
