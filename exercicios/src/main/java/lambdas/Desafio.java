8af755e2792da00c86bd1c7d825deea420f63612
GeorgeSalu <george.salu10@gmail.com>
2022-09-30 15:35:20 -0300

    lambdas - desafio

---
+++ exercicios/src/main/java/lambdas/Desafio.java
@@ -0,0 +1,17 @@
+package lambdas;
+
+public class Desafio {
+	public static void main(String[] args) {
+		
+		Produto p = new Produto("ipad", 3235.89, 0.19);
+		
+		/*
+		 1. a partir do produto calcular o preco real (com desconto)
+		 2. imposto municipal: >= 2500 (8.5%) / < 2500 (isento)
+		 3. frete: >= 3000 (100) / < 3000 (50)
+		 4. arredondar: deixar duas casas decimais
+		 5. formatar: r$ 1234.56 
+		*/
+		
+	}
+}
