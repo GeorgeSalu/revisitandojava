4769c126a1614a3458f353c2d3cc96f695974862
GeorgeSalu <george.salu10@gmail.com>
2022-09-30 09:47:47 -0300

    orientação a objetos - classe abstrata

--- exercicios/src/main/java/oo/polimorfismo/Jantar.java
+++ exercicios/src/main/java/oo/polimorfismo/Jantar.java
@@ -8,12 +8,14 @@ public class Jantar {
 		Arroz ingrediente1 = new Arroz(0.25);
 		Feijao ingrediente2 = new Feijao(0.189);
 		
+		Comida ingrediente3 = new Arroz(0.3);
+		
 		System.out.println(convidado.getPeso());
 		
 		convidado.comer(ingrediente1);
 		convidado.comer(ingrediente2);
+		convidado.comer(ingrediente3);
 		
 		System.out.println(convidado.getPeso());
-		
 	}
 }
