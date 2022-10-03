a97ff8828e8491363eccee65955beab750891662
GeorgeSalu <george.salu10@gmail.com>
2022-10-03 13:35:40 -0300

    stream - renomeando arquivo

--- exercicios/src/main/java/streams/Reduce1.java
+++ exercicios/src/main/java/streams/Reduce1.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.function.BinaryOperator;
 
-public class Reduce {
+public class Reduce1 {
 	public static void main(String[] args) {
 		
 		List<Integer> nums = Arrays.asList(1,2,3,4,5,6,7,8,9);
