d540906a159c62e9c4f416a1e96052fbfeeda094
GeorgeSalu <george.salu10@gmail.com>
2022-10-03 14:43:00 -0300

    stream - outro metodos

---
+++ exercicios/src/main/java/streams/Outros.java
@@ -0,0 +1,27 @@
+package streams;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Outros {
+	public static void main(String[] args) {
+		
+		Aluno a1 = new Aluno("Ana", 7.1);
+		Aluno a2 = new Aluno("Luna", 6.1);
+		Aluno a3 = new Aluno("Gui", 8.1);
+		Aluno a4 = new Aluno("Gabi", 10);
+		Aluno a5 = new Aluno("Gui", 8.1);
+		
+		List<Aluno> alunos = Arrays.asList(a1, a2, a3, a4, a5);
+		
+		System.out.println("distinct ... ");
+		alunos.stream().distinct().forEach(System.out::println);
+		
+		System.out.println("\nskip limit");
+		alunos.stream()
+			.distinct()
+			.skip(2)
+			.limit(2)
+			.forEach(System.out::println);
+	}
+}
