92d90e81d91149e66294410c9d20506993bef70b
GeorgeSalu <george.salu10@gmail.com>
2022-10-03 10:38:16 -0300

    stream - desafio map

---
+++ exercicios/src/main/java/streams/DesafioMap.java
@@ -0,0 +1,18 @@
+package streams;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class DesafioMap {
+	public static void main(String[] args) {
+		
+		List<Integer> nums = Arrays.asList(1,2,3,4,5,6,7,8,9,10);
+		
+		/*
+		1. numero para string binario .... 6 => "110" 
+		2. inverter a string... "110" => "011"
+		3. converter de volta para inteiro => "011" => 3
+		*/
+		
+	}
+}
