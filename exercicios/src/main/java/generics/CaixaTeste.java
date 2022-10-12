9942b5f5345c5f227d089ec726a6ab0143123759
GeorgeSalu <george.salu10@gmail.com>
2022-10-12 18:13:38 -0300

    generics - exemplo basico com generics

--- exercicios/src/main/java/generics/CaixaTeste.java
+++ exercicios/src/main/java/generics/CaixaTeste.java
@@ -3,7 +3,7 @@ package generics;
 public class CaixaTeste {
 	public static void main(String[] args) {
 		
-		Caixa<String> caixaA = new Caixa();
+		Caixa<String> caixaA = new Caixa<String>();
 		caixaA.aguardar("segredo");
 		
 		System.out.println(caixaA.abrir());
