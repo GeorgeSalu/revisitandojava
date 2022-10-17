7b3899f670893cea9c4cc8d5d0273ec3d73e4bdb
GeorgeSalu <george.salu10@gmail.com>
2022-10-17 11:49:56 -0300

    jpa - um para um #03

--- exercicios-jpa/src/main/java/modelo/umpraum/Assento.java
+++ exercicios-jpa/src/main/java/modelo/umpraum/Assento.java
@@ -4,6 +4,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 @Entity
@@ -15,6 +16,9 @@ public class Assento {
 	private Long id;
 
 	private String nome;
+	
+	@OneToOne(mappedBy = "assento")
+	private Cliente cliente;
 
 	public Assento() {
 		// TODO Auto-generated constructor stub
@@ -40,4 +44,13 @@ public class Assento {
 		this.nome = nome;
 	}
 
+	public Cliente getCliente() {
+		return cliente;
+	}
+
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+
+	
 }
