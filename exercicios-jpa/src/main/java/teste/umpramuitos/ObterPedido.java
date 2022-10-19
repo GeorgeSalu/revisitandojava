6ccc4e62834743cfc6a2ca68e2ad5ee6e962d697
GeorgeSalu <george.salu10@gmail.com>
2022-10-19 11:55:54 -0300

    jpa - um para muitos #02

---
+++ exercicios-jpa/src/main/java/teste/umpramuitos/ObterPedido.java
@@ -0,0 +1,19 @@
+package teste.umpramuitos;
+
+import infra.DAO;
+import modelo.umpramuitos.ItemPedido;
+import modelo.umpramuitos.Pedido;
+
+public class ObterPedido {
+	public static void main(String[] args) {
+
+		DAO<Pedido> dao = new DAO<>(Pedido.class);
+
+		Pedido pedido = dao.obterPorId(1l);
+
+		for (ItemPedido itemPedido : pedido.getItens()) {
+			System.out.println(itemPedido.getQuantidade());
+		}
+
+	}
+}
