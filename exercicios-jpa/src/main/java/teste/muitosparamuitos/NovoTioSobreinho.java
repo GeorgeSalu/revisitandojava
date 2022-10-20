96dd3cffa5eb8596cf11b37bcd418cb5fa98e7da
GeorgeSalu <george.salu10@gmail.com>
2022-10-20 14:07:51 -0300

    jpa - muitos para muitos #01

---
+++ exercicios-jpa/src/main/java/teste/muitosparamuitos/NovoTioSobreinho.java
@@ -0,0 +1,38 @@
+package teste.muitosparamuitos;
+
+import infra.DAO;
+import modelo.muitosparamuitos.Sobrinho;
+import modelo.muitosparamuitos.Tio;
+
+public class NovoTioSobreinho {
+	public static void main(String[] args) {
+		
+		Tio tia1 = new Tio("maria");
+		Tio tio2 = new Tio("joao");
+		
+		Sobrinho sobrinho1 = new Sobrinho("junior");
+		Sobrinho sobrinho2 = new Sobrinho("ana");
+		
+		tia1.getSobrinhos().add(sobrinho1);
+		sobrinho1.getTios().add(tia1);
+		
+		tia1.getSobrinhos().add(sobrinho2);
+		sobrinho2.getTios().add(tia1);
+		
+		tio2.getSobrinhos().add(sobrinho1);
+		sobrinho1.getTios().add(tio2);
+		
+		tio2.getSobrinhos().add(sobrinho2);
+		sobrinho2.getTios().add(tio2);
+		
+		DAO<Object> dao = new DAO<>();
+		dao.abrirT()
+			.incluir(tia1)
+			.incluir(tio2)
+			.incluir(sobrinho1)
+			.incluir(sobrinho2)
+			.fecharT()
+			.fechar();
+		
+	}
+}
