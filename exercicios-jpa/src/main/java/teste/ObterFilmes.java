dd94e12c196f664dd0cee24ca1a0d0508a15d574
GeorgeSalu <george.salu10@gmail.com>
2022-10-20 17:27:17 -0300

    jpa - named query

---
+++ exercicios-jpa/src/main/java/teste/ObterFilmes.java
@@ -0,0 +1,19 @@
+package teste;
+
+import java.util.List;
+
+import infra.DAO;
+import modelo.muitosparamuitos.Filme;
+
+public class ObterFilmes {
+	public static void main(String[] args) {
+		
+		DAO<Filme> dao = new DAO<>(Filme.class);
+		List<Filme> filmes = dao.consultar("filmesNotaMaiorQue", "nota", 8.0);
+		
+		for (Filme filme : filmes) {
+			System.out.println(filme.getNome());
+		}
+		
+	}
+}
