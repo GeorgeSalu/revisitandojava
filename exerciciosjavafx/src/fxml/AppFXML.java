bba6cb7ae89d8f1f5e4e9b57a5a6c9e15c083771
GeorgeSalu <george.salu10@gmail.com>
2022-11-24 15:33:18 -0300

    usando css com fxml

--- exerciciosjavafx/src/fxml/AppFXML.java
+++ exerciciosjavafx/src/fxml/AppFXML.java
@@ -12,10 +12,12 @@ public class AppFXML extends Application{
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
+		String arquivoCss = getClass().getResource("/fxml/Login.css").toExternalForm();
 		URL arquivoFXML = getClass().getResource("/fxml/Login.fxml");
 		GridPane raiz = FXMLLoader.load(arquivoFXML);
 		
 		Scene cena = new Scene(raiz, 350, 400);
+		cena.getStylesheets().add(arquivoCss);
 		
 		primaryStage.setResizable(false);
 		primaryStage.setTitle("Tela de Login");
