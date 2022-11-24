914cf47125193ba53e7d6cec2dd4fb007a7c848d
GeorgeSalu <george.salu10@gmail.com>
2022-11-23 21:51:29 -0300

    remove imports nao utilizados

--- exerciciosjavafx/src/layout/Quadrado.java
+++ exerciciosjavafx/src/layout/Quadrado.java
@@ -1,11 +1,5 @@
 package layout;
 
-import javafx.geometry.Insets;
-import javafx.geometry.Pos;
-import javafx.scene.control.Label;
-import javafx.scene.layout.Background;
-import javafx.scene.layout.BackgroundFill;
-import javafx.scene.layout.CornerRadii;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 
