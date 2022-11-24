a34524d6917beec78f2982d7bce57ff2d7b39056
GeorgeSalu <george.salu10@gmail.com>
2022-11-24 11:35:11 -0300

    FlowPane

--- exerciciosjavafx/src/layout/AppLayout.java
+++ exerciciosjavafx/src/layout/AppLayout.java
@@ -13,6 +13,7 @@ public class AppLayout extends Application {
 		
 //		Scene principal = new Scene(new TesteAnchorPane(), 800, 600);
 		raiz = new TesteBorderPane();
+		raiz = new TesteFlowPane();
 		
 		Scene principal = new Scene(raiz, 800, 600);
 		
